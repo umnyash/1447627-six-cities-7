bead17ef5090d775d29884fa765e6f8a0413572c
Rustam Sadreev <rustam17.sb@gmail.com>
2024-04-30 02:46:24 +0300

    Реализует приложение с CLI интерфейсом

---
+++ src/main.cli.ts
@@ -0,0 +1,15 @@
+import { CLIApplication, HelpCommand, ImportCommand, VersionCommand } from './cli/index.js';
+
+function bootstrap() {
+  const cliApplication = new CLIApplication();
+
+  cliApplication.registerCommands([
+    new HelpCommand(),
+    new ImportCommand(),
+    new VersionCommand(),
+  ]);
+
+  cliApplication.processCommand(process.argv);
+}
+
+bootstrap();
