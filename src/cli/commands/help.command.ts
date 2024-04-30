099b88d3e7134164f90fcd5e818319418449a3f5
Rustam Sadreev <rustam17.sb@gmail.com>
2024-04-30 04:01:44 +0300

    Раскрашивает информацию, выведенную в консоли командой --help.

--- src/cli/commands/help.command.ts
+++ src/cli/commands/help.command.ts
@@ -1,3 +1,4 @@
+import chalk from 'chalk';
 import { Command } from './command.interface.js';
 
 export class HelpCommand implements Command {
@@ -7,16 +8,16 @@ export class HelpCommand implements Command {
 
   public async execute(..._parameters: string[]): Promise<void> {
     console.info(`
-      Программа для подготовки данных для REST API сервера.
+      ${chalk.bgWhite.bold(' Программа для подготовки данных для REST API сервера. ')}
 
       Пример:
-          cli.js --<command> [--arguments]
+          ${chalk.hex('009d91')('cli.js')} ${chalk.hex('fffc00')('--<command>')} ${chalk.hex('ff6f00')('[--arguments]')}
 
       Команды:
-          --version:                  # выводит номер версии
-          --help:                     # печатает этот текст
-          --import <path>             # импортирует данные из TSV
-          --generate <n> <path> <url> # генерирует произвольное количество тестовых данных
+          ${chalk.hex('fffc00')('--version')}:                    # выводит номер версии
+          ${chalk.hex('fffc00')('--help')}:                       # печатает этот текст
+          ${chalk.hex('fffc00')('--import')} ${chalk.hex('ff6f00')('<path>')}:              # импортирует данные из TSV
+          ${chalk.hex('fffc00')('--generate')} ${chalk.hex('ff6f00')('<n> <path> <url>')}:  # генерирует произвольное количество тестовых данных
     `);
   }
 }
