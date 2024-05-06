20a2f76635d79bc47151aa0036c50717eb2912eb
Rustam Sadreev <rustam17.sb@gmail.com>
2024-05-06 03:36:08 +0300

    Реализует команду --generate.

--- src/cli/index.ts
+++ src/cli/index.ts
@@ -1,6 +1,7 @@
 export { CLIApplication } from './cli-application.js';
 export { CommandParser } from './command-parser.js';
 
+export { GenerateCommand } from './commands/generate.command.js';
 export { HelpCommand } from './commands/help.command.js';
 export { ImportCommand } from './commands/import.command.js';
 export { VersionCommand } from './commands/version.command.js';
