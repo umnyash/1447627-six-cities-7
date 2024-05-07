f76b6268ca4fc8700a9f92ce6e03ef780480e959
Rustam Sadreev <rustam17.sb@gmail.com>
2024-05-07 17:12:18 +0300

    Удаляет ненужный <string> в вызовах getRandomItem.

--- src/shared/libs/offer-generator/tsv-offer-generator.ts
+++ src/shared/libs/offer-generator/tsv-offer-generator.ts
@@ -15,24 +15,24 @@ export class TSVOfferGenerator implements OfferGenerator {
   constructor(private readonly mockData: MockServerData) { }
 
   public generate(): string {
-    const title = getRandomItem<string>(this.mockData.titles);
-    const description = getRandomItem<string>(this.mockData.descriptions);
-    const city = getRandomItem<string>(this.mockData.cities);
-    const previewPhoto = getRandomItem<string>(this.mockData.photos);
+    const title = getRandomItem(this.mockData.titles);
+    const description = getRandomItem(this.mockData.descriptions);
+    const city = getRandomItem(this.mockData.cities);
+    const previewPhoto = getRandomItem(this.mockData.photos);
     const photos = this.mockData.photos.join(';');
     const isPremium = Boolean(generateRandomValue(0, 1));
     const isFavorite = Boolean(generateRandomValue(0, 1));
     const rating = generateRandomValue(MIN_RATING, MAX_RATING, RATING_ACCURACY);
-    const housingType = getRandomItem<string>(this.mockData.housingTypes);
+    const housingType = getRandomItem(this.mockData.housingTypes);
     const numberOfRooms = generateRandomValue(MIN_NUMBERS_OF_ROOMS, MAX_NUMBER_OF_ROOMS);
     const numberOfGuests = generateRandomValue(MIN_NUMBERS_OF_GUESTS, MAX_NUMBER_OF_GUESTS);
     const price = generateRandomValue(MIN_PRICE, MAX_PRICE);
-    const amenities = getRandomItems<string>(this.mockData.amenities).join(';');
-    const userName = getRandomItem<string>(this.mockData.userNames);
-    const userEmail = getRandomItem<string>(this.mockData.userEmails);
+    const amenities = getRandomItems(this.mockData.amenities).join(';');
+    const userName = getRandomItem(this.mockData.userNames);
+    const userEmail = getRandomItem(this.mockData.userEmails);
     const userPassword = crypto.randomUUID();
-    const userType = getRandomItem<string>(this.mockData.userTypes);
-    const userAvatar = getRandomItem<string>(this.mockData.avatars);
+    const userType = getRandomItem(this.mockData.userTypes);
+    const userAvatar = getRandomItem(this.mockData.avatars);
 
     const postDate = dayjs()
       .subtract(generateRandomValue(FIRST_WEEK_DAY, LAST_WEEK_DAY), 'day')
